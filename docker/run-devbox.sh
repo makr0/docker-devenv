adea7a90c41d78b27175619c66ce2a900044d7ec
makr0 <makr0@r00tmakr0.org>
2014-10-24 09:07:15 +0000

    devbox: install cordova and ionic

--- docker/run-devbox.sh
+++ docker/run-devbox.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker rm devbox
-docker run -d -p 2222:22 --volumes-from data-container --name devbox crashc0de/devbox
+docker run -d -p 35729:35729 -p 8100:8100 -p 2222:22 -p 8888:8888 -p 8889:8889 --volumes-from data-container --name devbox crashc0de/devbox
 
