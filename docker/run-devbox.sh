c63082422a3c007602394cc8a76c7b0062b7c274
mdt <mdt@wegewerk.com>
2015-01-15 17:40:17 +0000

    vhost für konferenzapp-docs, run-devbox kommentiert

--- docker/run-devbox.sh
+++ docker/run-devbox.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 docker stop devbox && docker rm devbox
-docker run -d -p 35729:35729 -p 8100:8100 -p 2222:22 -p 8888:8888 -p 8889:8889 --volumes-from data-container --name devbox crashc0de/devbox
+
+# backticks used for comments. see http://stackoverflow.com/questions/9522631/bash-how-to-put-line-comment-for-a-multi-line-command#12797512
+docker run -d             `# ??` \
+           -p 2222:22     `# ssh` \
+           -p 35729:35729 `# livereload` \
+           -p 8888:8888   `# local server` \
+           -p 8889:8889   `# local server` \
+           -p 8100:8100   `# local server` \
+           --volumes-from data-container \
+           --name devbox \
+           -h devbox \
+           crashc0de/devbox
 
