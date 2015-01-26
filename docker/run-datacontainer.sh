1621b407a9eae2c9084319eb1af78aa1de240bb8
makr0 <makr0@r00tmakr0.org>
2015-01-26 23:55:37 +0000

    Babelext_box korrigiert

--- docker/run-datacontainer.sh
+++ docker/run-datacontainer.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+docker stop data-container && docker rm data-container;
 docker run -v /mnt/sda1/docker-data-container/:/data -v /mnt/sda1/docker-config-container/:/config --name data-container busybox true
--- docker/runall.sh
+++ docker/runall.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+./run-datacontainer.sh;
 ./run-webserver.sh;
 ./run-samba.sh;
 ./run-devbox.sh;
 ./run-couchdb.sh;
+./run-babelext_box.sh;
