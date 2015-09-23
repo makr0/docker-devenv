b74d4563e9a09d3ad012e691d87ce0ae21debce1
mdt <mdt@wegewerk.com>
2015-09-23 13:56:12 +0000

    androidsdk-box: 32bit libs installieren, build tools v22.0.1, rechte des ssh verzeichnis korrigiert

--- docker/dockerfiles/androidsdk/install_deps.sh
+++ docker/dockerfiles/androidsdk/install_deps.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+cd /root/
+
 #Android SDK
 ANDROID_SDK_FILENAME=android-sdk_r24-linux.tgz
 ANDROID_SDK=http://dl.google.com/android/$ANDROID_SDK_FILENAME
@@ -7,20 +9,21 @@ ANDROID_SDK=http://dl.google.com/android/$ANDROID_SDK_FILENAME
 #sudo apt-get install python-software-properties
 #sudo add-apt-repository ppa:webupd8team/java
 apt-get update
-apt-get install -y openjdk-7-jdk ant expect
+apt-get install -y openjdk-7-jdk expect
+apt-get install -y gcc-multilib lib32z1 lib32stdc++6
 
 curl -O $ANDROID_SDK
 tar -xzvf $ANDROID_SDK_FILENAME
 #sudo chown -R vagrant android-sdk-linux/
 #sudo chown -R vagrant:vagrant /root/.android/
 
-echo "ANDROID_HOME=~/android-sdk-linux" >> /root/.bashrc
-echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-i386" >> /root/.bashrc
+echo "export ANDROID_HOME=~/android-sdk-linux" >> /root/.bashrc
+echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64" >> /root/.bashrc
 echo "PATH=\$PATH:~/android-sdk-linux/tools:~/android-sdk-linux/platform-tools" >> /root/.bashrc
 
 expect -c '
 set timeout -1   ;
-spawn /root/android-sdk-linux/tools/android update sdk -u --all --filter platform-tool,android-22,build-tools-22.1.0
+spawn /root/android-sdk-linux/tools/android update sdk -u --all --filter platform-tool,android-22,build-tools-22.0.1
 expect {
     "Do you accept the license" { exp_send "y\r" ; exp_continue }
     eof
